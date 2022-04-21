f6c859a15d50110bff28516dac7793d237e4efa1
Carlos Evia <cevia@vt.edu>
2022-04-21 17:28:19 -0400

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,7 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
-        <span className="icon -love">
-          <RiHeart2Line />
-        </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+       Virginia Tech Hispanic and Latinx Caucus
       </p>
     </div>
   </footer>
